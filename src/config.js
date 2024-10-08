8b1f4b65783e26299be8d3fd2b2efa2b232353d5
Osato <osatohanmenogbeide1@gmail.com>
2024-10-08 03:36:46 +0100

    added live server

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const MB = 2;
 
 export const CONTRACT_ADDRESS = "0xf43980d57ae6d79d56f069cdaa351395610381ba";
 
-export const SERVER_URL = 'http://localhost:3030';
+export const SERVER_URL = 'https://stylus-web3-hackathon-backend.onrender.com';
 
 export const FRONTEND_URL = 'http://localhost:3000';
 
